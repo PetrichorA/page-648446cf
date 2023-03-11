db6624566bef488443187e0a0f169ac939215241
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2023-03-11 08:44:33 +0800

    0f5964b6-c065-486d-9f50-2a1a24a1805a

--- commit.py
+++ commit.py
@@ -4,13 +4,7 @@ from uuid import uuid4
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-git = 'C:/ArkData/misc/git.exe'
-
-subprocess.run(args=(git, 'add', '--all'))
-subprocess.run(args=(git, 'commit', '-m', str(uuid4())))
-subprocess.run(args=(git, 'push'))
-subprocess.run(args=(git, 'gc'))
-
-commit_hash = subprocess.check_output(
-    args=(git, 'rev-parse', 'HEAD')).decode().strip()
-input('https://cdn.jsdelivr.net/gh/PetrichorA/page-648446cf@{}/'.format(commit_hash))
+subprocess.run(args=('git', 'add', '--all'))
+subprocess.run(args=('git', 'commit', '-m', str(uuid4())))
+subprocess.run(args=('git', 'push'))
+subprocess.run(args=('git', 'gc'))
